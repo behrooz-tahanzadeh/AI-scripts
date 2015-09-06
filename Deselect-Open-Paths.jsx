a319603a23e889e5daa2c85c2407a0a5737a5340
Behrooz Tahanzadeh <behrooz.tahanzadeh@gmail.com>
2015-09-07 00:31:16 +0430

    adding deselect open path file

---
+++ Deselect-Open-Paths.jsx
@@ -0,0 +1,47 @@
+﻿/*
+***********************************************************
+	Remove Open Paths From Selection
+	
+	Developed by Behrooz Tahanzadeh (http://b-tz.com)
+	
+	Copyright 2014 License: GNU GPL v3 http://www.gnu.org/licenses/gpl-3.0.html
+	
+***********************************************************
+*/
+
+if(documents.length>0)
+{
+	if(activeDocument.selection.length>0)
+	{
+		var allPaths = activeDocument.selection;
+		var allPathsCount = allPaths.length;
+		var notPath = 0;
+		var notClosed = 0;
+		
+		if(confirm("Paths number:"+allPathsCount+"\nDo you want to continue?"))
+		{
+			for (var i=0; i<allPathsCount; i++)
+			{
+				if(allPaths[i].typename=="PathItem")
+				{
+					if(!allPaths[i].closed)
+					{
+						allPaths[i].selected = false;
+						notClosed++;
+					}
+				}
+				else
+				{
+					allPaths[i].selected = false;
+					notPath++;
+				}
+			}
+			
+			alert("Done!\n\nNumber of open paths: "+notClosed+"\nNumber of not path items: "+notPath);
+		}
+	}
+	else
+	{
+		alert("Select target path items and then run this script...!");
+	}
+}
